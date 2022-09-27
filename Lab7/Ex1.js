45b0cf4c663464201915eb1859c9c43397dd93c2
kaipielie <kai42@hawaii.edu>
2022-09-26 22:02:53 -1000

    Lab7 Unfinished

--- Lab7/Ex1.js
+++ Lab7/Ex1.js
@@ -1,9 +1,18 @@
 require("./products_data.js");
 var num_products = 5;
 var prod_num = 0
-while(++prod_num <= num_products) {
-    console.log(`${prod_num}. ${eval('name' + prod_num)}`);
-    if (prod_num > num_products*0.75 || prod_num < num_products*0.25) 
-    {console.log(`${prod_num}. ${eval('name' + prod_num)}`) + 'is sold out'; break};
+for (prod_num = 0; prod_num <= num_products; prod_num++) {
+    if (prod_num >= num_products/2) {
+        console.log("Don't ask for anything else!")
+        process.exit();
+    } else if ( (prod_num > 0.25*num_products) && (prod_num < 0.75*num_products) ) {
+        console.log(`${eval("typeof name" + i) != 'undefined'} is sold out!`);
+        continue;
+    }
+    console.log(`${prod_num}. ${eval("typeof name" + i) != 'undefined'}`);
 }
-// console.log("That's all we have!");
+console.log(`That's all we have!`);
+// while (++prod_num <= num_products) 
+// for (prod_num = 0; prod_num <= num_products; prod_num++)
+// ${eval("typeof name" + i) != 'undefined'}
+// ${eval('name' + prod_num )}
